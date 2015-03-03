fa59c46e2bfa1a482562129c21073cc1f96664e5
cheekyshroom <phernima@ucsc.edu>
2015-03-03 02:07:46 -0800

    Multiple random objects drawn now

--- main.fs
+++ main.fs
@@ -1,3 +1,4 @@
+include random.fs
 include ansi-term.fs
 BG_BLACK FG_WHITE ATTR_OFF 0 init-color-triple!
 
@@ -7,6 +8,9 @@ struct
 	char% field object-graphics
 	char% field object-symbol
 end-struct object%
+
+: objects  ( n -- n )
+	object% %size * ;
 
 : draw-object!  ( addr -- )
 	dup object-x @ over object-y @ move-cursor! \ move the cursor to the right place
@@ -29,16 +33,43 @@ end-struct object%
 : object-coordinates  ( addr -- x y )
 	dup object-x @ swap object-y @ ;
 
-\ our main player
-create player object% %allot
-\ initialize the player
-20 3 0 char @ player make-object!
+\ width and height of our "map"
+40 constant width
+20 constant height
 
+\ our main player
+16 constant object-count
+create game-objects object-count objects allot
+: index->object  ( n -- n )
+	objects game-objects + ;
+0 value controlled-object
+
+: for-objects!  ( xt -- )
+	object-count 0 do
+		i index->object
+		over execute
+	loop drop
+;
+
+: init-object!  { addr -- }
+	width randint height randint 0
+	27 randint [char] a +
+	addr
+	make-object!
+;
+
+: init-objects!  ( -- )
+	['] init-object! for-objects!
+;
+
+init-objects!
+
+\ 20 3 0 char @ controlled-object index->object make-object!
 : run!  { game-over -- }
 	begin
 		erase-display!
-		player draw-object!
-		player object-coordinates 2dup move-cursor!
+		['] draw-object! for-objects!
+		controlled-object index->object object-coordinates 2dup move-cursor!
 		key case
 			[char] k of 1- endof
 			[char] j of 1+ endof
@@ -46,7 +77,7 @@ create player object% %allot
 			[char] l of swap 1+ swap endof
 			[char] q of -1 to game-over endof
 		endcase
-		player move-object!
+		controlled-object index->object move-object!
 		graphics-normal!
 	game-over until
 ;
